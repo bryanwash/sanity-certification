e60f0d5f40827cb3150591f9018ddca9547715c3
beewash <beewash92@gmail.com>
2025-03-10 11:34:54 -0400

    feat: add subdirectories

---
+++ day-one-with-sanity-nextjs/src/sanity/client.ts
@@ -0,0 +1,8 @@
+import { createClient } from "next-sanity";
+
+export const client = createClient({
+  projectId: "y92r46rs",
+  dataset: "production",
+  apiVersion: "2024-11-01",
+  useCdn: false,
+});

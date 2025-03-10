6686dc66ac5a7c5c129525ccb7d247bb9756fc51
beewash <beewash92@gmail.com>
2025-03-10 16:52:07 -0400

    feat: presentation tool

--- day-one-with-sanity-nextjs/src/sanity/client.ts
+++ day-one-with-sanity-nextjs/src/sanity/client.ts
@@ -3,7 +3,12 @@ import { createClient } from "next-sanity";
 export const client = createClient({
   projectId: "y92r46rs",
   dataset: "production",
-  apiVersion: "2024-11-01",
+  apiVersion: "v2025-03-10",
   useCdn: false,
-  stega: { studioUrl: "/studio" },
+  stega: {
+    studioUrl:
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000"
+        : "https://sanity-certification-sigma.vercel.app/",
+  },
 });
--- day-one-with-sanity/sanity.config.ts
+++ day-one-with-sanity/sanity.config.ts
@@ -1,9 +1,15 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
+import {presentationTool} from 'sanity/presentation'
 import {schemaTypes} from './schemaTypes'
 import {structure} from './structure'
 import {defaultDocumentNode} from './structure/defaultDocumentNode'
+import {resolve} from './lib/presentation/resolve'
+export const BASE_URL =
+  process.env.NODE_ENV == 'development'
+    ? 'http://localhost:3000'
+    : 'https://sanity-certification-sigma.vercel.app'
 
 export default defineConfig({
   name: 'default',
@@ -12,7 +18,19 @@ export default defineConfig({
   projectId: 'y92r46rs',
   dataset: 'production',
 
-  plugins: [structureTool({structure, defaultDocumentNode}), visionTool()],
+  plugins: [
+    structureTool({structure, defaultDocumentNode}),
+    visionTool(),
+    presentationTool({
+      previewUrl: {
+        preview: BASE_URL,
+        draftMode: {
+          enable: `${BASE_URL}/api/draft-mode/enable`,
+        },
+      },
+      resolve,
+    }),
+  ],
 
   schema: {
     types: schemaTypes,
